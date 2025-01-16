01279233b53ec6149e901ac7800473c70c2f802b
Lukman Aditiya <fakea4988@gmail.com>
2025-01-16 09:31:56 +0800

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
+const NotesService = require("./services/posgres/NotesService");
 const NotesValidator = require("./validator/notes");
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ["*"],
@@ -28,6 +30,8 @@ const init = async () => {
   server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
+
+    console.log({ error: response.message });
 
     // penanganan client error secara internal.
     if (response instanceof ClientError) {
