8c4337abaa6270ffca9231d141387c28d1ea680c
Lukman Aditiya <fakea4988@gmail.com>
2025-01-19 12:16:35 +0800

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
 const notes = require("./api/notes");
 const NotesService = require("./services/posgres/NotesService");
@@ -9,6 +10,11 @@ const NotesValidator = require("./validator/notes");
 const users = require("./api/users");
 const UsersService = require("./services/posgres/UserService");
 const UsersValidator = require("./validator/users");
+
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/posgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
 
 const ClientError = require("./exceptions/ClientError");
 
@@ -25,6 +31,31 @@ const init = async () => {
 
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy("notesapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
 
   await server.register([
     {
@@ -39,6 +70,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
