499c96d9946e2eca2709f3d14fa01e26f018d34e
Lukman Aditiya <fakea4988@gmail.com>
2025-01-30 22:32:52 +0800

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -20,6 +20,10 @@ const collaborations = require("./api/collaborations");
 const CollaborationsService = require("./services/posgres/CollaborationsService");
 const CollaborationsValidator = require("./validator/collaborations");
 
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
@@ -92,6 +96,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
